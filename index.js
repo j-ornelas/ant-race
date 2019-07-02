c38b225f9c99c15cff2666a647a99b9a4880230c
john ornelas <johnnyoh1988@gmail.com>
2019-07-02 11:12:23 -0700

    feat(graphql) connect app to graphql endpoint

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
 import Root from './src/components/Root';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://antserver-blocjgjbpw.now.sh/graphql'
+});
+
 
 const App = () => (
-  <Root />
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>
 );
 
 AppRegistry.registerComponent('ants', () => App);
