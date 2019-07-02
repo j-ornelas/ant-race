36eaf55d2502fcd853f1a60470e19da25ab7b1c7
john ornelas <johnnyoh1988@gmail.com>
2019-07-02 11:05:05 -0700

    chore(dir) setup component directory

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Root from './src/components/Root';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => (
+  <Root />
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('ants', () => App);
---
+++ src/components/Root.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Text>this is the root component</Text>
+    );
+  }
+}
