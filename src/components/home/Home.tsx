77b2b538baca2b712e4b984d642632db68d2efec
john ornelas <johnnyoh1988@gmail.com>
2019-07-02 12:58:39 -0700

    feat(home) add header to home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HomeContainer } from './homeStyles';
 import { AntList } from '../antList/AntList';
 import { Footer } from '../footer/Footer';
+import { Header } from '../header/Header';
 import { Ant } from '../../interfaces';
 
 interface HomeProps {
@@ -11,6 +12,7 @@ export class Home extends Component<HomeProps> {
   render() {
     return (
       <HomeContainer>
+        <Header />
         <AntList ants={this.props.ants}/>
         <Footer />
       </HomeContainer>
